e221e04a32fa2cda7cd095185e21fa47c0e2d207
Colin Han <biz@colinhan.info>
2021-03-14 17:10:38 +0800

    Implement $5 * 2 = $10

--- src/lib/money.spec.ts
+++ src/lib/money.spec.ts
@@ -1,14 +1,11 @@
-import { double, power } from './money';
+import * as Money from './money';
 import {describe, it, expect} from "@jest/globals";
 
 describe('double', () => {
-  it('double 3 should be 6', () => {
-    expect(double(3)).toBe(6);
+  it('$5 * 2 = $10', () => {
+    const five = Money.dollar(5);
+    Money.times(five, 2);
+    expect(five.amount).toBe(10);
   })
 });
 
-describe('power', () => {
-  it ('2 power 4 should be 16', () => {
-    expect(power(2, 4)).toBe(16);
-  })
-});
